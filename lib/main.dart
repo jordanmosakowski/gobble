e01d5aac06d691bcf05fcd1f34991d2ff38b3370
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2022-02-24 08:45:44 -0800

    saves, better letter distribution

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:gobble/words.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
   final List<String> words = (await loadWords()).split('\n');
   for(int i = 0; i < words.length; i++) {
     words[i] = words[i].toLowerCase();
   }
   words.sort();
   print(words.length);
-  runApp(MyApp(words));
+  runApp(MyApp(words,prefs));
 }
 
 Future<String> loadWords() async {
@@ -21,24 +25,30 @@ Future<String> loadWords() async {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp(this.words,{Key? key}) : super(key: key);
+  MyApp(this.words,this.prefs,{Key? key}) : super(key: key);
   final List<String> words;
+  SharedPreferences prefs;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Gobble',
       theme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.red,
+        textTheme: GoogleFonts.latoTextTheme(
+          ThemeData(brightness: Brightness.dark).textTheme, // If this is not set, then ThemeData.light().textTheme is used.
+        ),
       ),
-      home: Home(words),
+      home: Home(words,prefs),
     );
   }
 }
 
 class Home extends StatefulWidget {
-  Home(this.words,{ Key? key }) : super(key: key);
+  Home(this.words,this.prefs,{ Key? key }) : super(key: key);
   List<String> words;
+  SharedPreferences prefs;
 
   @override
   _HomeState createState() => _HomeState();
@@ -48,26 +58,39 @@ class _HomeState extends State<Home> {
   int gridSize = 8;
   late List<String?> grid;
 
-  List<String> consanants = "bcdfghklmnprstvwy".split("");
-  List<String> uncommon = "jqxz".split("");
-  List<String> vowels = "aeiou".split("");
+  List<String> letters = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  List<double> weights = [
+    0.08457883369330453, 0.10885529157667387,
+    0.1500647948164147, 0.18401727861771058,
+    0.29053995680345573, 0.31041036717062637,
+    0.33727861771058315, 0.37088552915766737,
+    0.42885529157667385, 0.43118790496760256,
+    0.44933045356371487,  0.5114470842332614,
+    0.5387473002159827,  0.5884233261339092,
+    0.6535637149028077,  0.6852699784017278,
+    0.6877753779697623,  0.7654427645788335,
+    0.8232397408207341,  0.8862203023758097,
+    0.9265658747300214,  0.9397840172786175,
+      0.956630669546436,  0.9598272138228939,
+    0.9965442764578831,  1.0
+  ];
+
+  void save(){
+    widget.prefs.setStringList('save_grid', grid.map( (s) => s ?? "0").toList());
+    widget.prefs.setStringList("save_nextLetters", nextLetters);
+    widget.prefs.setInt("save_score", score);
+  }
 
   int score = 0;
 
   String chooseLetter(){
-    double chanceVowel = 0.3;
     double rand = Random().nextDouble();
-    if(rand < chanceVowel){
-      return vowels[Random().nextInt(vowels.length)];
+    int i = 0;
+    while(rand > weights[i] && i<letters.length){
+      i++;
     }
-    if(rand < chanceVowel + 0.05){
-      return uncommon[Random().nextInt(uncommon.length)];
-    }
-    return consanants[Random().nextInt(consanants.length)];
-    // if(score%5 == 4){
-    //   return vowels[Random().nextInt(vowels.length)];
-    // }
-    // return consanants[Random().nextInt(consanants.length)];
+    return letters[i];
   }
 
   late List<String> nextLetters;
@@ -88,21 +111,21 @@ class _HomeState extends State<Home> {
             children: const <Widget>[
               Text(
                 'How To Play',
-                style: TextStyle(fontSize: 25, color: Color.fromARGB(255, 255, 255, 255), fontWeight: FontWeight.bold,),
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold,),
                 textAlign: TextAlign.center,
-                
                 ),
-              Text('1.You Will Be Given A Letter To Place In The Grid',
+              Text('1. You Will Be Given A Letter To Place In The Grid',
               style: TextStyle(fontSize: 15),
-              
               ),
               Text('2. Place The Letter In A Grid Space',
               style: TextStyle(fontSize: 15),
-              
               ),
-              Text('3. Create 5 Letter Words To Remove Letters from the Grid',
+              Text('3. Create 4 and 5 letter words to remove letters from the grid',
               style: TextStyle(fontSize: 15),
-              )
+              ),
+              Text('4. Play until you fill up your grid',
+              style: TextStyle(fontSize: 15),
+              ),
             ],
           ),
         ),
@@ -128,8 +151,24 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    nextLetters = [chooseLetter(), chooseLetter(), chooseLetter()];
-    grid = List.filled(gridSize*gridSize, null);;
+    grid = List.filled(gridSize*gridSize, null);
+    if(widget.prefs.getStringList("save_grid") !=null){
+      List<String> tempGrid = widget.prefs.getStringList("save_grid") ?? [];
+      if(tempGrid.length == gridSize * gridSize){
+        grid = tempGrid.map((s) => s=="0" ? null : s).toList();
+      }
+    }
+    score = widget.prefs.getInt("save_score") ?? 0;
+    nextLetters = widget.prefs.getStringList("save_nextLetters") ?? [];
+    if(nextLetters.length!=3){
+      nextLetters = [chooseLetter(), chooseLetter(), chooseLetter()];
+    }
+
+    int highScore = widget.prefs.getInt("highScore") ?? 0;
+    if(highScore==0){
+      SchedulerBinding.instance?.addPostFrameCallback((_) => _showMyDialog());
+    }
+
   }
 
   List<FoundWord> findLen(int targetSize){
@@ -142,7 +181,7 @@ class _HomeState extends State<Home> {
           String word = letters.join();
           if(widget.words.contains(word)){
             print("FOUND: $word");
-            foundWords.add(FoundWord(true, i*gridSize+j, i*gridSize+j+targetSize));
+            foundWords.add(FoundWord(true,word, i*gridSize+j, i*gridSize+j+targetSize));
           }
         }
 
@@ -152,7 +191,7 @@ class _HomeState extends State<Home> {
           String word = letters.join();
           if(widget.words.contains(word)){
             print("FOUND: $word");
-            foundWords.add(FoundWord(false, j*gridSize+i, (j+targetSize)*gridSize + i));
+            foundWords.add(FoundWord(false,word, j*gridSize+i, (j+targetSize)*gridSize + i));
           }
         }
       }
@@ -163,6 +202,7 @@ class _HomeState extends State<Home> {
   void findWords(){
     List<FoundWord> foundWords = [...findLen(4),...findLen(5)];
     if(foundWords.isEmpty){
+      save();
       checkLoss();
       return;
     }
@@ -180,6 +220,13 @@ class _HomeState extends State<Home> {
         }
       }
     });
+
+    save();
+    // SnackBar snackBar = SnackBar(
+    //   content: Text('You spelled: ${foundWords.map((f) => f.word).join(", ")}'),
+    //   duration: const Duration(milliseconds: 2500),
+    // );
+    // ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   bool showedLoseDialog = false;
@@ -189,6 +236,9 @@ class _HomeState extends State<Home> {
       return;
     }
     showedLoseDialog = true;
+    widget.prefs.remove("save_grid");
+    widget.prefs.remove("save_nextLetters");
+    widget.prefs.remove("save_score");
     showDialog(
       context: context,
       builder: (BuildContext context){
@@ -220,7 +270,13 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Gobble'),
-        leading:IconButton(onPressed: _showMyDialog, icon: Icon(Icons.help))
+        leading:IconButton(onPressed: _showMyDialog, icon: Icon(Icons.help)),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Center(child: Text("High score: ${widget.prefs.getInt("highScore") ?? 0}", style: Theme.of(context).textTheme.headline6,)),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.only(top: 25.0),
@@ -251,6 +307,9 @@ class _HomeState extends State<Home> {
                               setState(() {
                                 grid[i] = nextLetters.removeAt(0);
                                 score++;
+                                if(score>(widget.prefs.getInt("highScore") ?? 0)){
+                                  widget.prefs.setInt("highScore", score);
+                                }
                                 nextLetters.add(chooseLetter());
                                 findWords();
                               });
@@ -315,8 +374,9 @@ class WordTile extends StatelessWidget {
 }
 
 class FoundWord{
-  FoundWord(this.row,this.start,this.end);
+  FoundWord(this.row,this.word,this.start,this.end);
   final bool row;
+  final String word;
   final int start;
   final int end;
 }
